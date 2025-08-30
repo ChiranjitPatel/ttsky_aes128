7f30d8666c94c27c9fee36470ccda99cd4ee0c2e
chiranjitpatel <chiranjitpatel08@gmail.com>
2025-08-30 16:37:47 +0530

    case_trial

--- src/tt_um_sub_bytes.v
+++ src/tt_um_sub_bytes.v
@@ -2,16 +2,17 @@ module tt_um_sub_bytes (
     input [7:0] in_data,    // 128-bit input
     output [7:0] out_data   // 128-bit output
 );
-    reg [7:0] sbox [0:255]; // Declare the S-box as a 256-entry array
+    // reg [7:0] sbox [0:255]; // Declare the S-box as a 256-entry array
 
     // Initialize the S-box from a hex file
-    initial begin
-        // $readmemh("D:/Github/ttsky_aes128/src/sbox.hex", sbox); // Point to your regular S-box file
-        $readmemh("src/sbox.hex", sbox); // Point to your regular S-box file
-    end
+    // initial begin
+        // // $readmemh("D:/Github/ttsky_aes128/src/sbox.hex", sbox); // Point to your regular S-box file
+        // $readmemh("src/sbox.hex", sbox); // Point to your regular S-box file
+    // end
 
     // Apply the S-box transformation to each byte of the input
-    assign out_data[7:0]    = sbox[in_data[7:0]];
+    // assign out_data[7:0]    = sbox[in_data[7:0]];
+	sbox u4(in_data[7:0], out_data[7:0]); 
     // assign out_data[15:8]   = sbox[in_data[15:8]];
     // assign out_data[23:16]  = sbox[in_data[23:16]];
     // assign out_data[31:24]  = sbox[in_data[31:24]];
